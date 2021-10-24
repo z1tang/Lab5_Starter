6510c1490294a2bf71ca6476710cc83ab7148557
Jiayi Zhao <vikizjy@gmail.com>
2021-10-24 15:20:23 -0700

    revert explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,23 +3,5 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
-  // change image
-  var drop = document.getElementById('horn-select');
-  var image = document.querySelector('img[src*="image"]');
-
-  drop.addEventListener('change', (event) => {
-    var horn = event.target.value;
-    image.src = ('assets/images/'+ horn +'.svg');
-  });
-  
-  // play sound
-  var btn = document.querySelector('button');
-  var sound = document.getElementsByClassName('hidden');
-  btn.addEventListener('click', (event) => {
-    sound.src = ('assets/audio/' + horn + '.mp3');
-    sound.play();
-  });
-
 
 }
